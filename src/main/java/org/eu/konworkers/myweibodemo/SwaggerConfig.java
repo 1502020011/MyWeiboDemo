54115fb70cd0f62db25c4a9d96921e848d6cea26
kyou <heqiaokyou@hotmail.com>
2023-06-08 19:50:02 +0800

    1

--- src/main/java/org/eu/konworkers/myweibodemo/SwaggerConfig.java
+++ src/main/java/org/eu/konworkers/myweibodemo/SwaggerConfig.java
@@ -11,24 +11,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 
-//@Configuration
-//@EnableSwagger2
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
-    public Docket docket(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .groupName("main");
-    }
-
-    private ApiInfo apiInfo(){
-        return new ApiInfo("MyWeiboDemo Api Documentation",
-                "MyWeiboDemo Api Documentation",
-                "0.0.1",
-                "",
-                new Contact("kyou","",""),
-                "Apache 2.0",
-                "http://www.apache.org/licenses/LICENSE-2.0",
-                new ArrayList());
-    }
+//    public Docket docket(){
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo())
+//                .groupName("main");
+//    }
+//
+//    private ApiInfo apiInfo(){
+//        return new ApiInfo("MyWeiboDemo Api Documentation",
+//                "MyWeiboDemo Api Documentation",
+//                "0.0.1",
+//                "",
+//                new Contact("kyou","",""),
+//                "Apache 2.0",
+//                "http://www.apache.org/licenses/LICENSE-2.0",
+//                new ArrayList());
+//    }
 }
