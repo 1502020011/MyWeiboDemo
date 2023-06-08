e0439846d7c0f83345eb25e75290617bb352c90b
kyou <heqiaokyou@hotmail.com>
2023-06-08 19:19:33 +0800

    1

---
+++ src/main/java/org/eu/konworkers/myweibodemo/domain/enties/MessageConstants.java
@@ -0,0 +1,12 @@
+package org.eu.konworkers.myweibodemo.domain.enties;
+
+public class MessageConstants {
+    public static final String MESSAGE_GET_SUCCESS = "获取消息成功";
+    public static final String MESSAGE_GET_FAIL = "获取消息失败";
+    public static final String USER_GET_SUCCESS = "获取用户成功";
+    public static final String USER_GET_FAIL = "获取用户失败";
+    public static final String MESSAGE_POST_SUCCESS = "发布消息成功";
+    public static final String MESSAGE_POST_FAIL = "发布消息失败";
+    public static final String USER_EDIT_SUCCESS = "用户修改成功";
+    public static final String USER_EDIT_FAIL = "用户修改失败";
+}
