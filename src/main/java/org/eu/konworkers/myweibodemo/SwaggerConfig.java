636e3529abf898a644d9d239e0a43cfad99206b6
kyou <heqiaokyou@hotmail.com>
2023-06-08 20:00:48 +0800

    1

--- src/main/java/org/eu/konworkers/myweibodemo/SwaggerConfig.java
+++ src/main/java/org/eu/konworkers/myweibodemo/SwaggerConfig.java
@@ -7,9 +7,12 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
 //    public Docket docket(){
