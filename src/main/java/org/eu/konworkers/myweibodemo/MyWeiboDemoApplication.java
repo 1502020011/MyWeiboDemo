c5f0947cf9ae3fde632aed4601d16ae24822778d
kyou <heqiaokyou@hotmail.com>
2023-06-08 18:52:56 +0800

    1

--- src/main/java/org/eu/konworkers/myweibodemo/MyWeiboDemoApplication.java
+++ src/main/java/org/eu/konworkers/myweibodemo/MyWeiboDemoApplication.java
@@ -2,8 +2,9 @@ package org.eu.konworkers.myweibodemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MyWeiboDemoApplication {
 
 	public static void main(String[] args) {
