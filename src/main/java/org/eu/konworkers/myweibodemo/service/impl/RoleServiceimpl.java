0c9f8a95e09c1e08fdbd2337cf1bf560c01f169b
kyou <heqiaokyou@hotmail.com>
2023-06-09 14:17:05 +0800

    1

--- src/main/java/org/eu/konworkers/myweibodemo/service/impl/RoleServiceimpl.java
+++ src/main/java/org/eu/konworkers/myweibodemo/service/impl/RoleServiceimpl.java
@@ -5,10 +5,12 @@ import org.eu.konworkers.myweibodemo.domain.pojo.Role;
 import org.eu.konworkers.myweibodemo.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class RoleServiceimpl implements RoleService {
 
     @Autowired
